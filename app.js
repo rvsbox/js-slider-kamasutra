f93d41d609e62076d6eb70ab5e1c70c3a8b91351
rvsbox <rvsbox7@gmail.com>
2020-08-08 23:09:06 +0200

    a21 FN Show image

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const showPrevBtn = document.getElementById("show-prev-btn");
 const showNextBtn = document.getElementById("show-next-btn");
+const slideImage = document.getElementById("slide-img");
 
 showPrevBtn.addEventListener("click", onShowPrevBtnClick);
 showNextBtn.addEventListener("click", onShowNextBtnClick);
+
+let imagesPaths = [];
+imagesPaths.push('images/tiger.jpg');
+imagesPaths.push('images/ninja.jpg');
+imagesPaths.push('images/moon.jpg');
+imagesPaths.push('images/road.jpg');
+imagesPaths.push('images/bat-man.jpg');
+
+slideImage.src =  imagesPaths[0];
 
 
 function onShowPrevBtnClick() {
