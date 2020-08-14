1d4f5cb3e9bd96c53eed694bde1cfa8c0ffc4a24
rvsbox <rvsbox7@gmail.com>
2020-08-14 17:34:28 +0200

    sl03 OOP Added setTimeout to SliderDataService.js

--- js/SliderDataService.js
+++ js/SliderDataService.js
@@ -3,10 +3,14 @@ function SliderDataService() {
     this.getUrls = function () {
         let _imagesPaths = [];
 
-        _imagesPaths.push("images/ninja.jpg");
-        _imagesPaths.push("images/jubei.gif");
-        _imagesPaths.push("images/moon.jpg");
-        _imagesPaths.push("images/bat-man.jpg");
+        // массив заполнится через 3 сек
+        setTimeout(function () {
+            _imagesPaths.push("images/ninja.jpg");
+            _imagesPaths.push("images/jubei.gif");
+            _imagesPaths.push("images/moon.jpg");
+            _imagesPaths.push("images/bat-man.jpg");
+        }, 3000)
+
 
         return _imagesPaths;
     }
